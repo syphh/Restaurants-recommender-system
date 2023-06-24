8062f90917494c79b9ae49b65e78685abf4bb57a
OnnaMira <spiritofgamermirall@gmail.com>
2023-06-24 17:18:04 +0100

    added the application

---
+++ streamlit_app/Home.py
@@ -0,0 +1,254 @@
+import streamlit as st
+import streamlit as st
+from streamlit_folium import st_folium,folium_static
+import folium
+from streamlit_option_menu import option_menu
+from utils import *
+
+
+st.set_page_config(page_title='Restaurants Recommendation System', page_icon='🍔', layout='wide', initial_sidebar_state='auto')
+
+local_css("style.css")
+
+
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+if "df_resto" and "df_users"  not in st.session_state:
+     st.session_state.df_resto, st.session_state.df_users,  = load_data('../data/')
+
+if "ncfmodel" not in st.session_state:
+    st.session_state.ncfmodel = load_ncfmodel()
+
+if "word2vecmodel" not in st.session_state:
+    st.session_state.word2vecmodel = load_word2vecmodel()
+
+if "all_categories" not in st.session_state:
+    all_categories = pd.read_csv('../data/unique_category_updated.csv')
+    st.session_state.all_categories = all_categories['category']
+     
+
+selected = option_menu(
+        menu_title =None,
+        options = ['Home','Explorer les tendances', 'Tester utilisateurs existants', 'Nouvel utilisateur'],
+        icons= ['house', 'person-lines-fill', 'person-plus'],
+        default_index = 0,
+        orientation='horizontal',
+    )
+
+if selected == 'Home':
+    st.session_state.count = 0
+    right_column, left_column = st.columns([3,1.2])
+    original_title = '<p style="font-family:Impact; color:rgb(238, 78, 78); font-size: 150px; margin-left:20px; margin-top:80px;">RESTAURANT </p>'
+    title = '<p style="font-family:Fantasy; font-size: 100px; margin-left: 20px; ">RECOMMENDER</p>'
+    right_column.markdown(original_title, unsafe_allow_html=True)
+    right_column.markdown(title, unsafe_allow_html=True)
+    left_column.image('pizzmm.png',width=600)
+    text = '<p style="font-family:serif; font-size: 20px; margin-left: 20px;margin-right:150px;">Bienvenue dans notre application de recommandation de restaurants realisée dans le cadre de notre projet de fin d\'études, ici vous pouvez explorer les tendances du moments, tester les modèles avec des utilisateurs déja inscrits ainsi qu\'intoduire et tester un nouvel utilisateur. </p>'
+    right_column.markdown(text, unsafe_allow_html=True)
+
+
+if selected == 'Explorer les tendances':
+    
+    st.header('Dans cette section vous pouvez explorer les tendances des restaurants en ce moment')
+    #st.selectbox('Sort by',options=['Best rated','Most popular','Price range','Closest'])
+   
+    
+    restos_pop = st.session_state.df_resto.iloc[:200] 
+    st.session_state.df_resto['popularity_score'] = popularity_score(st.session_state.df_resto) 
+    restos_pop = st.session_state.df_resto.sort_values(by='popularity_score',ascending=False)
+    if 'count' not in st.session_state:
+        st.session_state.count = 0
+   
+    
+    b1,f,b2 = st.columns([2,15.5,1])
+    with b2:
+        reset =st.button('Reset')
+        if reset:
+            
+            st.session_state.count =0
+            print('rested.?? '+ str(st.session_state.count))
+        
+    with b1:
+        More_button = st.button('Show More')
+    
+    if More_button:
+        print('more')
+
+    placeholder = st.empty()
+    placeholder.grid = make_grid(4,4)
+    print(st.session_state.count)
+    for i in range(2):
+                    for j in range(4):
+                        with placeholder.grid[i][j]:
+                           
+                            show_results(restos_pop, st.session_state.count,expanded=True)
+                            st.session_state.count+=1
+
+ 
+
+if selected == 'Tester utilisateurs existants':
+    st.session_state.count = 0
+    header = st.container()
+    dataset = st.container()
+    features = st.container()
+    modelTraining = st.container()
+
+    with header:
+        st.header('Dans cette section vous pouvez recommander des restaurants à des utilisateurs existants')
+ 
+    with modelTraining:
+       
+        
+        
+      
+        user_selected= st.selectbox('Choisir un utilisateur',options= st.session_state.df_users['user_id'].head(100).values)
+        
+        st.write('You selected user:',user_selected)
+        Predicted = st.button('Predict')
+        print('selected' , user_selected)
+        map_l,mi,resto_r = st.columns([2,0.25,2])
+        
+        
+        if Predicted:
+            
+            resto_r.header('**Top Restaurants**')
+            print('Predicted = ')
+ 
+            user_id = user_selected
+            prediction_score,history_cats_user = recommend_to_existing_user(st.session_state.df_resto, st.session_state.df_users, user_id, st.session_state.ncfmodel,st.session_state.word2vecmodel,st.session_state.all_categories)
+            print(prediction_score)
+            for i in range( prediction_score.shape[0]):
+                with resto_r:
+                    show_results(prediction_score,i)
+
+            
+            
+            map_l.write('**Map des top restaurants**')
+            #Cmap(prediction_score[['latitude','longitude']])
+        
+            with map_l:
+                        map_res = display_results_on_map(prediction_score,  st.session_state.df_users[st.session_state.df_users['user_id'] == user_id][['latitude','longitude']])
+                        
+                        st_data = folium_static(map_res)
+                        st.write('**Historique des catégories préférées de l\'utilisateur:**')
+                        
+                        st.dataframe(history_cats_user)
+
+
+
+if selected == 'Nouvel utilisateur':
+    st.session_state.count = 0
+    
+    header = st.container()
+    dataset = st.container()
+    features = st.container()
+    modelTraining = st.container()
+
+    with header:
+        st.header('Avoir une recommandation pour un nouvel utilisateur')
+    
+
+    with modelTraining:
+
+ 
+        if 'session_lat' and 'session_long' not in st.session_state:
+            st.session_state.session_lat = 33.69
+            st.session_state.session_long = -118.69
+        if 'selected_categories'  not in st.session_state:
+            st.session_state.selected_categories = ['italianrestaurant']
+        
+        if 'prediction_score' not in st.session_state:
+            st.session_state.prediction_score = pd.DataFrame()
+
+        if 'selected_price' not in st.session_state:
+            st.session_state.selected_price = 1
+        st.write('**Test a new user:**')
+        print('top restart??')
+        
+        L, M,R = st.columns(([2,0.25,2]))
+        with R:
+            Latitude,Longitude,selected_categories =33.775541,-118.171894,None
+
+           
+            
+           
+            print('creation map')
+            m = folium.Map(location=[36.7783, -119.4179], zoom_start=5)
+                    
+            m.add_child(folium.LatLngPopup())
+            
+            f_map = st_folium(m,width = 800, height = 600)
+                    
+            selected_latitude = Latitude
+            selected_longitude = Longitude
+                
+            if f_map.get("last_clicked"):
+                        selected_latitude = f_map["last_clicked"]["lat"]
+                        selected_longitude = f_map["last_clicked"]["lng"]
+
+        with L:
+            
+            prix = st.slider('Select a price level',  1,1, 4)
+            st.session_state.selected_price = prix
+            st.success(f"Prix selectionné: {st.session_state.selected_price}")
+            st.write('Selectionner vos categories preferees')
+            selected_categories = st.multiselect(label='Categories',options= st.session_state.all_categories)
+            st.write('Clicker sur une location sur la carte pour selectionner la position')
+            form = st.form("Position entry form")
+           
+            submit = form.form_submit_button()
+       
+            if submit:
+                if selected_latitude== Latitude and selected_longitude == Longitude:
+                        st.warning("Selected position has default values!")
+                        print('warning same loc as default')
+                else:
+                        st.success(f"Position selectionnée: {selected_latitude}, {selected_longitude}")
+             
+                st.session_state.session_lat = round(selected_latitude,6)
+                st.session_state.session_long = round(selected_longitude,6)
+              
+                if selected_categories == []:
+                      
+                        st.warning("Veuillez selectionner une categorie(s)!")
+                else:
+                      st.session_state.selected_categories = selected_categories
+                      st.success(f"categories choisies: {selected_categories}")
+                print('stored' + str(st.session_state.session_lat) +' '+ str(st.session_state.session_long))
+               
+               
+            Predicted2 = st.button('Predict')
+            
+        map_l2,mi2,resto_r2 = st.columns([2,0.25,2])
+            
+        
+
+        #user_profile = {'latitude': Latitude,'longitude' :Longitude, 'avg_price': prix, 'categories': selected_categories}
+        print('start of if predict')
+        if Predicted2:
+              
+                print('saved prof = '+str(st.session_state.session_lat), str(st.session_state.session_long),4,str(st.session_state.selected_categories))
+                st.session_state.prediction_score =recommend_to_new_user(st.session_state.df_resto,  st.session_state.session_lat, st.session_state.session_long,st.session_state.selected_price,st.session_state.selected_categories,st.session_state.word2vecmodel,st.session_state.all_categories)
+                print('Predicted = ')
+                
+                print(st.session_state.prediction_score)
+                resto_r2.header('**Top Restaurants**')
+                
+        c1, boo,c2 =  st.columns([2,0.25,2])
+                
+        if st.session_state.prediction_score.shape[0] == 0:
+                    resto_r2.write('No restaurants found nearby')
+        else:
+                
+                with c1:
+                        for i in range( st.session_state.prediction_score.shape[0]):
+                           
+                            show_results( st.session_state.prediction_score, i)
+                    
+                print('where')
+                with c2:
+                        map_res = display_results_on_map(st.session_state.prediction_score,[st.session_state.session_lat, st.session_state.session_long])
+                        
+                    
+                        st_data = folium_static(map_res,width=800, height=600)
+                        map_l2.write('**Map des top restaurants**')
+
---
+++ streamlit_app/utils.py
@@ -0,0 +1,225 @@
+import streamlit as st
+import streamlit as st
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from collections import defaultdict, Counter
+from gensim.models import Word2Vec
+import folium
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+
+def popularity_score(df_resto, m=50):  # m is the number of reviews starting from which the restaurant mean rating becomes more reliable that the global mean rating
+    avg_rating = df_resto['avg_rating'].mean()  # mean rating of all restaurants
+    # calculate the popularity score for each restaurant
+    result = (df_resto['num_of_reviews']/(df_resto['num_of_reviews']+m))*df_resto['avg_rating']\
+        + (m/(df_resto['num_of_reviews']+m))*avg_rating
+    return result
+
+def get_category_score(df_resto, sim_map):  # sim_map is a dictionary that contains the similarity between each category and the user's preferences
+    try:
+        return df_resto['category'].apply(lambda x: np.mean([sim_map[c] for c in x]))  # return the mean similarity between the restaurant's categories and the user's preferences
+    except:
+        return np.zeros(len(df_resto))
+@st.cache_data
+def preprocess_df_resto(df_resto):
+    df_resto['category'] = df_resto['category'].apply(lambda x: x.lower().replace(' ', '').replace('[', '').replace(']', '').replace("'", '').split(','))
+    return df_resto
+@st.cache_data
+def preprocess_df_users(df_users):
+    df_users['list_of_resto'] = df_users['list_of_resto'].apply(eval)
+    df_users = df_users[df_users['latitude'].notnull()]
+    return df_users
+
+def knowledge_score(df_resto, sim_map, user_price, user_lat_norm, user_lng_norm, weights):
+    results = df_resto[['gmap_id']].copy()
+    results['price_sim'] = 1-df_resto['price'].apply(lambda x: np.abs(x-user_price))/3   # similarity between restaurants prices and the user's preferred price, normalized to [0,1]
+    results['category_sim'] = get_category_score(df_resto, sim_map)  # similarity between restaurants categories and the user's preferred categories, normalized to [0,1]
+    results['location_sim'] = 1-np.sqrt((df_resto['norm_lat']-user_lat_norm)**2 + (df_resto['norm_lng']-user_lng_norm)**2)  # similarity between restaurants locations and the user's selected location, normalized to [0,1]
+    results['score'] = results['price_sim']*weights['price'] + results['category_sim']*weights['category'] + results['location_sim']*weights['location']  # weighted sum of the three similarities
+    results['score'] *= 5  # normalize the score to [0,5]
+    return results['score']
+
+def content_based_score(df_resto, sim_map, user_price_hist, user_lat_hist, user_lng_hist, weights):
+    results = df_resto[['gmap_id']].copy()
+    results['price_sim'] = 1-df_resto['price'].apply(lambda x: np.abs(x-user_price_hist))/3  # similarity between restaurants prices and the user's history price, normalized to [0,1]
+    results['category_sim'] = get_category_score(df_resto, sim_map)  # similarity between restaurants categories and the user's history categories, normalized to [0,1]
+    results['location_sim'] = 1-np.sqrt((df_resto['norm_lat']-user_lat_hist)**2 + (df_resto['norm_lng']-user_lng_hist)**2)  # similarity between restaurants locations and the user's history locations, normalized to [0,1]
+    results['score'] = results['price_sim']*weights['price'] + results['category_sim']*weights['category'] + results['location_sim']*weights['location']  # weighted sum of the three similarities
+    results['score'] *= 5
+    return results['score']
+
+def popularity_rec_weight(n):  # n is the number of interactions of the user with the system
+    return 0.95 - 0.9/(1+np.exp(-0.5*n))  # weight attributed to the popularity score given the number of interactions
+
+def knowledge_rec_weight(n):
+    return 0.85 - 0.7/(1+np.exp(-0.5*n))  # weight attributed to the knowledge score given the number of interactions
+
+def content_rec_weight(n):
+    return 1.6/(1+np.exp(-0.5*n)) - 0.8/(1+np.exp(-0.05*n)) - 0.4  # weight attributed to the content score given the number of interactions
+
+def collab_rec_weight(n):
+    return 0.8/(1+np.exp(-0.05*n)) - 0.4  # weight attributed to the collaborative score given the number of interactions
+
+def recommend_to_new_user(df_resto, user_lat, user_lng, user_price, user_categories, model_wv, categories, r=0.05, k=10):  # r is the radius of the circle around the user's location in which the restaurants are selected, and k the number of restaurants to recommend 
+    # normalize the user's location
+    min_lat, max_lat = df_resto['latitude'].min(), df_resto['latitude'].max()
+    min_lng, max_lng = df_resto['longitude'].min(), df_resto['longitude'].max()
+    user_lat_norm = (user_lat - min_lat)/(max_lat - min_lat)  # normalize the user's latitude
+    user_lng_norm = (user_lng - min_lng)/(max_lng - min_lng)  # normalize the user's longitude
+    
+    if 'restaurant' in categories:
+        categories.remove('restaurant')  # remove the generic category 'restaurant'
+    fav_cats = {k: 1/len(user_categories) for k in user_categories}  # normalize the weights to sum to 1
+    print(f'User info :\npreferred price: {round(user_price, 2)}\nlocation: ({user_lat}, {user_lng})\npreferred categories: {user_categories}')
+
+    # compute the similarity between the user's favorite categories and the restaurants' categories
+    sim_map = defaultdict(lambda: 0)
+    sim_map.update({c: np.sum([model_wv.wv.similarity(c, k)*fav_cats[k] for k in fav_cats]) for c in categories if c in model_wv.wv.key_to_index})
+
+    # get the restaurants in the circle around the user's location
+    min_nb_resto_around = 100
+    df_resto['distance'] = np.sqrt((df_resto['latitude']-user_lat)**2 + (df_resto['longitude']-user_lng)**2)
+    if  df_resto[df_resto['distance'] <= r].shape[0] >= min_nb_resto_around:
+        df_resto = df_resto[df_resto['distance'] <= r].copy()
+    else:
+        df_resto = df_resto.sort_values(by='distance', ascending=True).head(min_nb_resto_around).copy()
+
+    weights = {'popu_rec': 0.1, 'knowledge_rec': 0.9}
+    
+    # compute the score of each restaurant given the recommendation methods
+    df_resto['popularity_score'] = popularity_score(df_resto) if weights['popu_rec'] > 0 else 0
+    df_resto['knowledge_score'] = knowledge_score(df_resto, sim_map, user_price, user_lat_norm, user_lng_norm, {'price': 0.2, 'location': 0.3, 'category': 0.5}) if weights['knowledge_rec'] > 0 else 0
+
+    # compute the final score of each restaurant with a weighted sum of the scores
+    df_resto['score'] = df_resto['popularity_score']*weights['popu_rec'] + df_resto['knowledge_score']*weights['knowledge_rec']
+    return df_resto.sort_values(by='score', ascending=False).head(k)  # return the k best restaurants
+
+def content_based_score(df_resto, sim_map, user_price_hist, user_lat_hist, user_lng_hist, weights):
+    results = df_resto[['gmap_id']].copy()
+    results['price_sim'] = 1-df_resto['price'].apply(lambda x: np.abs(x-user_price_hist))/3
+    results['category_sim'] = get_category_score(df_resto, sim_map)
+    results['location_sim'] = 1-np.sqrt((df_resto['norm_lat']-user_lat_hist)**2 + (df_resto['norm_lng']-user_lng_hist)**2)
+    results['score'] = results['price_sim']*weights['price'] + results['category_sim']*weights['category'] + results['location_sim']*weights['location']
+    results['score'] *= 5
+    return results['score']
+
+def collab_score(df_resto, user_code, model):
+    nb_resto = df_resto.shape[0]
+    result = df_resto[['gmap_id']].copy()
+    result['score'] = model.predict([np.array([user_code]*nb_resto), df_resto['resto_code']]).reshape(-1)
+    return result['score']
+
+def recommend_to_existing_user(df_resto, df_user, user_id, model_ncf, model_wv, categories,  r=0.1, k=10):
+    resto_codes = set(range(92363))
+    user = df_user[df_user['user_id'] == user_id].iloc[0]
+    user_price = user['avg_price']
+    user_lat = user['latitude']
+    user_lng = user['longitude']
+    min_lat, max_lat = df_resto['latitude'].min(), df_resto['latitude'].max()
+    min_lng, max_lng = df_resto['longitude'].min(), df_resto['longitude'].max()
+    user_lat_norm = (user_lat - min_lat)/(max_lat - min_lat)
+    user_lng_norm = (user_lng - min_lng)/(max_lng - min_lng)
+    user_code = user['user_code']
+    history = user['list_of_resto']
+    fav_cats = Counter()
+    df_history = df_resto[df_resto['gmap_id'].isin(history)]
+    df_history['category'].apply(lambda x: fav_cats.update(Counter(x)))
+    if 'restaurant' in fav_cats:
+        del fav_cats['restaurant']
+    fav_cats = {k:v for k, v in fav_cats.items() if k in sorted(fav_cats, key=fav_cats.get, reverse=True)[:5]}
+    print(fav_cats)
+    fav_cats = {k: v/sum(fav_cats.values()) for k, v in fav_cats.items()}
+    sim_map = defaultdict(lambda: 0)
+    sim_map.update({c: np.sum([model_wv.wv.similarity(c, k)*fav_cats[k] for k in fav_cats]) for c in categories if c in model_wv.wv.key_to_index})
+    n = len(history)
+    weights = {'popu_rec': popularity_rec_weight(n), 'knowledge_rec': knowledge_rec_weight(n), 'content_rec': content_rec_weight(n), 'collab_rec': collab_rec_weight(n)}
+    #weights = {'popu_rec': 0, 'knowledge_rec': 0.5, 'content_rec': 0.5, 'collab_rec': 0}
+    df_resto['distance'] = np.sqrt((df_resto['latitude']-user_lat)**2 + (df_resto['longitude']-user_lng)**2)
+    df_resto = df_resto[(df_resto['distance'] <= r)&(df_resto['resto_code'].isin(resto_codes))].copy()
+    print(df_resto.shape)
+    df_resto['popularity_score'] = popularity_score(df_resto) if weights['popu_rec'] > 0 else 0
+    df_resto['knowledge_score'] = knowledge_score(df_resto, sim_map, user_price, user_lat_norm, user_lng_norm, {'price': 0.1, 'location': 0.4, 'category': 0.5}) if weights['knowledge_rec'] > 0 else 0
+    df_resto['content_score'] = content_based_score(df_resto, sim_map, user_price, user_lat_norm, user_lng_norm, {'price': 0.3, 'category': 0.6, 'location': 0.1}) if weights['content_rec'] > 0 else 0
+    df_resto['collab_score'] = collab_score(df_resto, user_code, model_ncf) if weights['collab_rec'] > 0 else 0
+    df_resto['score'] = df_resto['popularity_score']*weights['popu_rec'] + df_resto['knowledge_score']*weights['knowledge_rec'] + df_resto['content_score']*weights['content_rec'] + df_resto['collab_score']*weights['collab_rec']
+    df_history_cats = pd.DataFrame.from_dict(fav_cats, orient='index', columns=['taux d\'occurences'])
+    return df_resto.sort_values(by='score', ascending=False).head(k), df_history_cats
+
+@st.cache_data
+def load_data(path):
+            folder = path
+            df_resto = pd.read_csv(folder + 'restaurants.csv')
+            df_users = pd.read_csv(folder + 'users.csv')
+           
+       
+            df_resto = preprocess_df_resto(df_resto)
+            df_users = preprocess_df_users(df_users)
+            return df_resto, df_users
+@st.cache_resource
+def load_ncfmodel():
+            model =tf.keras.models.load_model('../models/ncf_model.h5')
+            return model
+
+@st.cache_resource 
+def load_word2vecmodel():
+            model = Word2Vec.load('../models/word2vec.model')
+            return model
+
+def show_results(prediction_score, i,expanded = False):
+ 
+                            
+                            expander = st.expander(label =str (i+1)+' ' + prediction_score['name'].iloc[i] ,expanded=expanded)
+                                
+                            description  = prediction_score['description'].iloc[i]
+                            if str(description) == 'nan':    
+                                expander.write('**Restaurant**')
+                            else:
+                                expander.write('**Description**:' + str(description))
+                            cat= (prediction_score['category'].iloc[i])
+                            expander.write('**Category**:' + str(cat))
+                            expander.write('**Price range**: ' + str(prediction_score['price'].iloc[i])+' 💲')
+                            expander.write('**Average rating** :'+ str(round(prediction_score['avg_rating'].iloc[i]))+'⭐' )
+                                #insert rating as star emoji
+                            expander.write('**Address**: '+str(prediction_score['address'].iloc[i]))
+                            expander.write('**Google maps url**: '+ str(prediction_score['url'].iloc[i]))
+
+def display_results_on_map(prediction_score,  location):
+    
+                        newmap = folium.Map(location=location, 
+                        zoom_start=8, control_scale=True)
+
+                    
+                        for i,row in prediction_score.iterrows():
+                            #Setup the content of the popup
+                            iframe = folium.IFrame('Name :' + str(row["name"]),height=80,width=120)
+                            
+                            #Initialise the popup using the iframe
+                            popup = folium.Popup(iframe, min_width=300, max_width=300)
+                            
+                            #Add each row to the map
+                            folium.Marker(location=[row['latitude'],row['longitude']],
+                                        popup = popup, c=row['name']).add_to(newmap)
+                            
+                        iframe = folium.IFrame('Utilisateur',height=80,width=120)
+                            
+                            #Initialise the popup using the iframe
+                        popup = folium.Popup(iframe, min_width=300, max_width=300)
+                        folium.Marker(location=location,
+                                        popup = popup, c=r'Position utilisateur',icon= folium.Icon(color='red')).add_to(newmap)
+
+                        return newmap
+
+
+def make_grid(cols,rows):
+    grid = [0]*cols
+    for i in range(cols):
+        with st.container():
+            grid[i] = st.columns(rows)
+    return grid
+#""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
